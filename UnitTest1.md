186391b57f3faf57710bb81133016404e8d91af3
Vormamim <81339757+Vormamim@users.noreply.github.com>
2023-03-07 19:40:28 +1100

    base files

---
+++ UnitTest1.md
@@ -0,0 +1,7 @@
+Write a Python function that takes an array of integers and returns the highest number that appears more than once in the array.
+
+Function Signature: `def highest_duplicate_number(arr: List[int]) -> int:`
+
+Example:
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 11, 12, 12, 13, 14, 15, 16, 16]
+assert highest_duplicate_number(arr) == 16
---
+++ UnitTest1.py
@@ -0,0 +1,20 @@
+# Sample Unit Test
+
+import unittest
+
+class TestHighestDuplicateNumber(unittest.TestCase):
+    
+    def test_duplicate_number(self):
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 11, 12, 12, 13, 14, 15, 16, 16]
+        self.assertEqual(highest_duplicate_number(arr), 16)
+        
+    def test_no_duplicate(self):
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        self.assertEqual(highest_duplicate_number(arr), -1)
+        
+    def test_empty_array(self):
+        arr = []
+        self.assertEqual(highest_duplicate_number(arr), -1)
+        
+if __name__ == '__main__':
+    unittest.main()
